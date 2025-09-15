874aced8953d2c00a4b19bd1092a3b8663d1df88
Devpateldevlop <devpatel190703@gmail.com>
2025-09-15 15:21:16 +0530

    commit

---
+++ model/categories.js
@@ -0,0 +1,18 @@
+
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://deva:devpatel1234@cluster0.pf0qwiv.mongodb.net/")
+.then(() => console.log('MongoDB Connected...'))
+.catch((err) => console.log('MongoDB connection error: ' + err));
+
+
+const categorySchema = new mongoose.Schema({
+  catid: { type: String, required: true, unique: true }, // e.g., "kurtis"
+  name: { type: String, required: true, unique: true }, // e.g., "Kurtis"
+  description: { type: String }, // optional
+  image: { type: String }, // category banner
+}, { timestamps: true });
+
+
+const Category = mongoose.model('categories', categorySchema,'categories');
+module.exports = Category;
