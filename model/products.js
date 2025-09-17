78f61d0afb2dc084b0a053665a90ca4e0c0ac36a
Devpateldevlop <devpatel190703@gmail.com>
2025-09-17 12:50:21 +0530

    Reso

--- model/products.js
+++ model/products.js
@@ -8,10 +8,10 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true }, // e.g., "Floral Cotton Kurti"
   description: { type: String },
   price: { type: Number, required: true },
-  size: [{ type: String }], // ["S", "M", "L", "XL"]
-  color: [{ type: String }], // ["Red", "Blue"]
+  size: { type: String }, // ["S", "M", "L", "XL"]
+  color: { type: String }, // ["Red", "Blue"]
   stock: { type: Number, default: 0 },
-  images: [{ type: String }], // product images
+  images: { type: String }, // product images
   categoryname: { 
     type: String, 
     required: true 
